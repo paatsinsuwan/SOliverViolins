1b39781ae193625a9432a5bcf0aedcad3efd43b7
Paat Sinsuwan <paat@sinsuwan.com>
2011-12-05 15:49:53 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SergioOliver::Application.config.secret_token = 'f2a0b290372412f546f9334343bc868d110d845ba8f7b79d0c07ec6525a1547ebbca1491b0e1dbfc6bcdd0f4d7feb2690f2da672e69d48247aa5dc2ace45291f'
